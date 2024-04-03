c0af324b3d068aa2a85335a7760ea8c51a54f552
jmarco <jean.espinoza@kibernum.com.pe>
2024-04-03 01:07:52 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
 
-import Prueba from "./Components/Prueba/Prueba";
+import Prueba from "./components/Prueba/Prueba";
 import "./App.css";
 import { useMediaQuery, useTheme } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css"; // Importar los estilos CSS de Bootstrap
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React, { useEffect } from "react";
 
-import Header from "./Components/header";
-import { BeforeAfter } from "./Components/beforeAfter/BeforeAfter";
+import Header from "./components/header";
+import { BeforeAfter } from "./components/beforeAfter/BeforeAfter";
 import imagenAntigua from "./assets/img/HeroSection-old.png";
 import imagenNueva from "./assets/img/HeroSection-new.png";
 import imagenAntiguaMobile from "./assets/img/mobile-habi-old.png";
 import imagenNuevaMobile from "./assets/img/mobile-habi.png";
-// import Simulator from "./Components/simulator";
-import StcResultado from "./Components/stcResultado";
-import StcFormulario from "./Components/stcFormulario";
-import Footer from "./Components/footer";
+// import Simulator from "./components/simulator";
+import StcResultado from "./components/stcResultado";
+import StcFormulario from "./components/stcFormulario";
+import Footer from "./components/footer";
 
 function App() {
   const theme = useTheme();
