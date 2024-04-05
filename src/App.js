8c682eb305861445640e1e1a7f376a450e231911
jmrk02 <jeanmarcoe2@gmail.com>
2024-04-04 21:01:51 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
 
-import Prueba from "./Components/Prueba/Prueba";
+import Prueba from "./sections/Prueba/Prueba";
 import "./App.css";
 import { useMediaQuery, useTheme } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css"; // Importar los estilos CSS de Bootstrap
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React, { useEffect } from "react";
 
-import Header from "./Components/header";
-import { BeforeAfter } from "./Components/beforeAfter/BeforeAfter";
+import Header from "./sections/header";
+import { BeforeAfter } from "./sections/beforeAfter/BeforeAfter";
 import imagenAntigua from "./assets/img/HeroSection-old.png";
 import imagenNueva from "./assets/img/HeroSection-new.png";
 import imagenAntiguaMobile from "./assets/img/mobile-habi-old.png";
 import imagenNuevaMobile from "./assets/img/mobile-habi.png";
-// import Simulator from "./Components/simulator";
-import StcResultado from "./Components/stcResultado";
-import StcFormulario from "./Components/stcFormulario";
-import Footer from "./Components/footer";
+// import Simulator from "./sections/simulator";
+import StcResultado from "./sections/stcResultado";
+import StcFormulario from "./sections/stcFormulario";
+import Footer from "./sections/footer";
 import RentabilidadState from "./context/rentabilidad/rentabilidadState";
 
 function App() {
